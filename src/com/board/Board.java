5b270a6facc7c3123110a17dd15b1bbe7e9b63d3
kikascacador@hotmail.com <kikascacador@hotmail.com@0491c951-b032-77ce-06e8-0e643281a8ed>
2015-02-23 10:25:31 +0000

    Version control

--- src/com/board/Board.java
+++ src/com/board/Board.java
@@ -4,26 +4,27 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Board {
-	
+
 	private static final int WIDTH = 600;
 	private static final int HEIGHT = 600;
+	private static final double VERSION = 1.0;
 
 	private JFrame puzzle_frame;
 	private JPanel puzzle_panel;
-	
+
 	public Board() {
-		puzzle_frame = new JFrame("Puzzle");
+		puzzle_frame = new JFrame("Client Puzzle (Version " + VERSION + ")");
 		puzzle_frame.setSize(WIDTH, HEIGHT);
 		puzzle_frame.setResizable(false);
 		puzzle_frame.add(fullPanel());
 		puzzle_frame.setLocationRelativeTo(null);
 		puzzle_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+
 	public void initGUI() {
 		puzzle_frame.setVisible(true);
 	}
-	
+
 	public JPanel fullPanel() {
 		puzzle_panel = new JPanel();
 		return puzzle_panel;
