8ba7c646007e26d60e88afde595ed22ad2746f02
kikascacador@hotmail.com <kikascacador@hotmail.com@0491c951-b032-77ce-06e8-0e643281a8ed>
2015-02-25 14:21:42 +0000

    hjjhhg

--- src/com/algorithms/Node.java
+++ src/com/algorithms/Node.java
@@ -4,10 +4,17 @@ public class Node {
 
 	private boolean visited;
 	private String current_node_data;
+	private int estimated_score;
 
 	public Node(boolean visited, String current_node_data) {
 		this.visited = visited;
 		this.current_node_data = current_node_data;
+	}
+	
+	public Node(boolean visited, String current_node_data, int estimated_score) {
+		this.visited = visited;
+		this.current_node_data = current_node_data;
+		this.estimated_score = estimated_score;
 	}
 
 	public boolean isVisited() {
@@ -18,12 +25,20 @@ public class Node {
 		return current_node_data;
 	}
 	
+	public int getEstimated_score() {
+		return estimated_score;
+	}
+	
 	public void setVisited(boolean visited) {
 		this.visited = visited;
 	}
 	
 	public void setCurrent_node_data(String current_node_data) {
 		this.current_node_data = current_node_data;
+	}
+	
+	public void setEstimated_score(int estimated_score) {
+		this.estimated_score = estimated_score;
 	}
 
 }
