410fdf3c5597f340cffe392f3ca1cc5a40fccca0
kikascacador@hotmail.com <kikascacador@hotmail.com@0491c951-b032-77ce-06e8-0e643281a8ed>
2015-04-14 16:59:03 +0000

    aha

---
+++ src/com/logger/LoggerDebugger.java
@@ -0,0 +1,39 @@
+package com.logger;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LoggerDebugger {
+
+	private Logger logger;
+	private FileHandler file_handler;
+
+	public LoggerDebugger() {
+		try {
+			logger = Logger.getLogger("8-Puzzle - Log!");
+			file_handler = new FileHandler(System.getProperty("user.dir"));
+			logger.addHandler(file_handler);
+			SimpleFormatter formatter = new SimpleFormatter();
+			file_handler.setFormatter(formatter);
+		} catch (SecurityException | IOException e) {
+			logger.severe(e.getMessage());
+		}
+	}
+
+	public void saveLog(String log, String type) {
+		/** REVIEW **/
+		switch (type) {
+		case "info":
+			logger.info(log);
+			break;
+		case "warning":
+			logger.severe(log);
+			break;
+		default:
+			break;
+		}
+	}
+
+}
